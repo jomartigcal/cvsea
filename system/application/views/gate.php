92df3fae74633eefc606accec4344925cf01af6e
Jomar Tigcal <jomartigcal@gmail.com>
2011-03-08 00:25:16 +0800

    Updating title of some pages to "CvSEA"

--- system/application/views/gate.php
+++ system/application/views/gate.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Halalan - Gate - <?php echo $title; ?></title>
+	<title>CvSEA - Gate - <?php echo $title; ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>public/stylesheets/gate.css" />
 	<script type="text/javascript" src="<?php echo base_url(); ?>public/javascripts/jquery.js"></script>
